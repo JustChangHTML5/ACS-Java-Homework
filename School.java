2544a8527659a5ea5d2f423ebad0d20c14eb279c
JustChangHTML5 <80505615+JustChangHTML5@users.noreply.github.com>
2022-06-25 07:42:08 -0600

    update files

--- School.java
+++ School.java
@@ -6,7 +6,17 @@ public class School {
       Student kristie = new Student(1, "Kristie", "Lan", 'F', 8, 4.0f);
       System.out.println("student: " + " " + kristie.getId() + " " + kristie.getFirstname() + " " + kristie.getLastname() + " " + kristie.getGender() + " " + kristie.getGrade() + " " + kristie.getGpa());
       
-      Teacher charlie = new Teacher(101, "Charlie", "Gao", 'M', "Professer", 10.0f);
+      Teacher charlie1 = new Teacher(101, "Charlie", "Gao", 'M', "Professer", 10.0f);
+      System.out.println(charlie1.getId() + " " + charlie1.getFirstname() + " " + charlie1.getLastname() + " " + charlie1.getGender() + " " + charlie1.getPosition() + " " + charlie1.getSalary());
+      
+      Teacher charlie = new Teacher();
+      charlie.setId(101);
+      charlie.setFirstname("Charlie");
+      charlie.setLastname("Gao");
+      charlie.setGender('M');
+      charlie.setPosition("Professer");
+      charlie.setSalary(10.0f);
+      System.out.println(charlie.getId() + " " + charlie.getFirstname() + " " + charlie.getLastname() + " " + charlie.getGender() + " " + charlie.getPosition() + " " + charlie.getSalary());
       
       Student eric = new Student();
       eric.setId(2);
@@ -15,7 +25,7 @@ public class School {
       eric.setGender('M');
       eric.setGrade(7);
       eric.setGpa(4.0f);
-      //System.out.println("student: " + " " + eric.getId() + " " + eric.getFirstname() + " " + eric.getLastname() + " " + eric.getGender() + " " + eric.getGrade() + " " + eric.getGpa());
+      System.out.println("student: " + " " + eric.getId() + " " + eric.getFirstname() + " " + eric.getLastname() + " " + eric.getGender() + " " + eric.getGrade() + " " + eric.getGpa());
         
       System.out.println("Student info: " + eric.toString());   
    }
