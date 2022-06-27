fff8dd49ce4fbc685a06e3d22c2f21def8e12a43
JustChangHTML5 <80505615+JustChangHTML5@users.noreply.github.com>
2022-06-27 15:31:22 -0600

    Add files via upload

--- School.java
+++ School.java
@@ -1,7 +1,69 @@
+
+import model.Student;
+import model.Teacher;
+import model.Course;
+import model.Registration;
+import model.Department;
+
 public class School {
+   
+   private String name;
+   private String address;
+   private String city;
+   private String state;
+   private int numOfFaculty;
+   
+   public School() {
+   
+   }
+   
+   public School(String name, String address, String city, String state, int numOfFaculty) {
+      this.name = name;
+      this.address = address;
+      this.city = city;
+      this.state = state;
+      this.numOfFaculty = numOfFaculty;
+   }
 
    public static void main(String[] args) {
+   
       System.out.println("Welcome to my school");
+      
+      int size = args.length;
+      System.out.println("args: " + size);
+      
+      if (size >= 7) {
+         System.out.println("6 args");
+         String type = args[0];
+         String idStr = args[1];
+         String firstname = args[2];
+         String lastname = args[3];
+         String genderStr = args[4];
+         String arg4 = args[5];
+         String arg5 = args[6];
+         
+         int id = Integer.parseInt(idStr);
+         char genderChar = genderStr.charAt(0);
+         
+         Object obj = null;
+         if (type.equals("Student")) {
+         
+            int grade = Integer.parseInt(arg4);
+            float gpa = Float.parseFloat(arg5);
+            obj = new Student(id, firstname, lastname, genderChar, grade, gpa);
+            
+         } else if (type.equals("Teacher")) {
+            
+            String position = arg4;
+            float salary = Float.parseFloat(arg5);
+            obj = new Teacher(id, firstname, lastname, genderChar, position, salary);
+         
+         }
+         
+         System.out.println(obj.toString());
+      } else {
+         System.out.println("No args");
+      }
       
       Student kristie = new Student(1, "Kristie", "Lan", 'F', 8, 4.0f);
       System.out.println("student: " + " " + kristie.getId() + " " + kristie.getFirstname() + " " + kristie.getLastname() + " " + kristie.getGender() + " " + kristie.getGrade() + " " + kristie.getGpa());
